168bb2bac2d47c6b09a0936356e60053a17ee62e
Adam Nemeth <nemethadam88@gmail.com>
2022-02-10 10:19:28 +0100

    improve month parser

--- src/utility.ts
+++ src/utility.ts
@@ -1,10 +1,56 @@
+const january = ["jan", "jan.", "január", "1", "01"];
+const february = ["feb", "febr", "február", "2", "02"];
+const march = ["mar", "marc", "már", "márc", "március", "3", "03"];
+const april = ["apr", "ápr", "április", "4", "04"];
+const may = ["may", "maj", "máj", "május", "5", "05"];
+const june = ["feb", "febr.", "február", "2", "02"];
+const july = ["feb", "febr.", "február", "2", "02"];
+const august = ["feb", "febr.", "február", "2", "02"];
+const september = [
+  "sep",
+  "sept",
+  "szep",
+  "szept",
+  "september",
+  "szeptember",
+  "9",
+  "09",
+];
+const october = ["oct", "okt", "október", "october", "10", "10"];
+const november = ["feb", "febr.", "február", "2", "02"];
+const december = ["feb", "febr.", "február", "2", "02"];
+
 export const getMonthNumber = (monthAsString: string): number => {
+  const trimmedMonth = monthAsString
+    .trim()
+    .replace(".", "")
+    .toLocaleLowerCase();
+
   switch (true) {
-    case monthAsString.toLocaleLowerCase() === "január":
+    case january.includes(trimmedMonth):
       return 1;
-    case monthAsString.toLocaleLowerCase() === "február" ||
-      monthAsString.toLocaleLowerCase() === "febr.":
+    case february.includes(trimmedMonth):
       return 2;
+    case march.includes(trimmedMonth):
+      return 3;
+    case april.includes(trimmedMonth):
+      return 4;
+    case may.includes(trimmedMonth):
+      return 5;
+    case june.includes(trimmedMonth):
+      return 6;
+    case july.includes(trimmedMonth):
+      return 7;
+    case august.includes(trimmedMonth):
+      return 8;
+    case september.includes(trimmedMonth):
+      return 9;
+    case october.includes(trimmedMonth):
+      return 10;
+    case november.includes(trimmedMonth):
+      return 11;
+    case december.includes(trimmedMonth):
+      return 12;
     default:
       return 0;
   }
