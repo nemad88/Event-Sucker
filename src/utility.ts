bbeeb4c6946722aceb7af411180f16a6e7f9bb16
Adam Nemeth <nemethadam88@gmail.com>
2022-03-08 10:16:37 +0100

    fix months

--- src/utility.ts
+++ src/utility.ts
@@ -1,24 +1,26 @@
-const january = ["jan", "jan.", "január", "1", "01"];
-const february = ["feb", "febr", "február", "2", "02"];
-const march = ["mar", "marc", "már", "márc", "március", "3", "03"];
-const april = ["apr", "ápr", "április", "4", "04"];
-const may = ["may", "maj", "máj", "május", "5", "05"];
-const june = ["jun", "june", "jún", "júni", "június", "6", "06"];
-const july = ["jul", "july", "júl", "júli", "7", "07"];
-const august = ["aug", "augusztus", "august", "8", "08"];
-const september = [
-  "sep",
-  "sept",
-  "szep",
-  "szept",
-  "september",
-  "szeptember",
-  "9",
-  "09",
-];
-const october = ["oct", "okt", "október", "october", "10", "10"];
-const november = ["nov", "november", "11"];
-const december = ["dec", "december", "12"];
+const months = {
+  january: ["jan", "jan.", "január", "1", "01"],
+  february: ["feb", "febr", "február", "2", "02"],
+  march: ["mar", "marc", "már", "márc", "március", "3", "03"],
+  april: ["apr", "ápr", "április", "4", "04"],
+  may: ["may", "maj", "máj", "május", "5", "05"],
+  june: ["jun", "june", "jún", "júni", "június", "6", "06"],
+  july: ["jul", "july", "júl", "júli", "július", "7", "07"],
+  august: ["aug", "augusztus", "august", "8", "08"],
+  september: [
+    "sep",
+    "sept",
+    "szep",
+    "szept",
+    "september",
+    "szeptember",
+    "9",
+    "09",
+  ],
+  october: ["oct", "okt", "október", "october", "10", "10"],
+  november: ["nov", "november", "11"],
+  december: ["dec", "december", "12"],
+};
 
 export const getMonthNumber = (monthAsString: string): number => {
   const trimmedMonth = monthAsString
@@ -27,29 +29,29 @@ export const getMonthNumber = (monthAsString: string): number => {
     .toLocaleLowerCase();
 
   switch (true) {
-    case january.includes(trimmedMonth):
+    case months.january.includes(trimmedMonth):
       return 1;
-    case february.includes(trimmedMonth):
+    case months.february.includes(trimmedMonth):
       return 2;
-    case march.includes(trimmedMonth):
+    case months.march.includes(trimmedMonth):
       return 3;
-    case april.includes(trimmedMonth):
+    case months.april.includes(trimmedMonth):
       return 4;
-    case may.includes(trimmedMonth):
+    case months.may.includes(trimmedMonth):
       return 5;
-    case june.includes(trimmedMonth):
+    case months.june.includes(trimmedMonth):
       return 6;
-    case july.includes(trimmedMonth):
+    case months.july.includes(trimmedMonth):
       return 7;
-    case august.includes(trimmedMonth):
+    case months.august.includes(trimmedMonth):
       return 8;
-    case september.includes(trimmedMonth):
+    case months.september.includes(trimmedMonth):
       return 9;
-    case october.includes(trimmedMonth):
+    case months.october.includes(trimmedMonth):
       return 10;
-    case november.includes(trimmedMonth):
+    case months.november.includes(trimmedMonth):
       return 11;
-    case december.includes(trimmedMonth):
+    case months.december.includes(trimmedMonth):
       return 12;
     default:
       return 0;
